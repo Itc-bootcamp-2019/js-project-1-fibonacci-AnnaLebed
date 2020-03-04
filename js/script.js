1c082a59047fb69af46e08d7ba9a9065c4cc73a7
Anna <annettlebid@gmail.com>
2020-03-04 16:19:38 +0200

    Milestone 3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-document.getElementById("x").innerHTML = "8";
-document.getElementById("y").innerHTML = "21";
+document.getElementById("x").innerHTML = "7";
+
+function numberFibonacci(number) {
+  let previous1 = 0,
+    previous2 = 1,
+    result = 1;
+
+  for (let i = 2; i <= number; i++) {
+    result = previous1 + previous2;
+    previous1 = previous2;
+    previous2 = result;
+  }
+  return result;
+}
+
+const calculation = numberFibonacci(5);
+document.getElementById("y").innerHTML = numberFibonacci(7);
