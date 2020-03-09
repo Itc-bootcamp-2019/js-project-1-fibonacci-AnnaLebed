e66662baf30b5a18c142462c55490fe94486ec38
Anna <annettlebid@gmail.com>
2020-03-09 14:34:28 +0200

    Milestone 6

--- js/script.js
+++ js/script.js
@@ -1,18 +1,117 @@
-const myBtn = document.getElementById("buttonCalc")
-myBtn.addEventListener("click", numberFibonacci);
+// Milestone 4
 
+// function getInputValue() {
+//     const inputValue = document.getElementById("inputX").value;
+//     return inputValue;
+// }
 
-function numberFibonacci() {
-    let number = document.getElementById("Xnumber").value;
-    let previous1 = 0,
-        previous2 = 1,
-        result = 1;
+// function FibonacciValue() {
 
-    for (let i = 2; i <= number; i++) {
-        result = previous1 + previous2;
-        previous1 = previous2;
-        previous2 = result;
+//     const inputValue = document.getElementById("inputX").value
+//     let previous1 = 0,
+//         previous2 = 1,
+//         resultValue = 1;
+
+//     for (let i = 2; i <= inputValue; i++) {
+//         resultValue = previous1 + previous2;
+//         previous1 = previous2;
+//         previous2 = resultValue;
+//     }
+//     document.getElementById("resultY").innerHTML = resultValue
+//     console.log(resultValue);
+// }
+
+// function getInputValue() {
+//     const inputValue = document.getElementById("inputX").value
+//     let previous1 = 0,
+//         previous2 = 1,
+//         resultValue = 1;
+
+//     for (let i = 2; i <= inputValue; i++) {
+//         resultValue = previous1 + previous2;
+//         previous1 = previous2;
+//         previous2 = resultValue;
+//     }
+//     document.getElementById("resultY").innerHTML = resultValue
+//     console.log(resultValue);
+// }
+
+// Milestone 4.1
+
+// *******************************************
+// function FibonacciValue(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+//     return (FibonacciValue(n - 1) + FibonacciValue(n - 2));
+// };
+// console.log(FibonacciValue(7))
+
+// *****************************************
+
+// const n = document.getElementById("inputX").value;
+// console.log(n);
+// const n = document.getElementById("inputX").value;
+
+// function FibonacciValue() {
+//     const n = document.getElementById("inputX").value;
+//     if (n < 2) {
+//         // return n;
+//         // console.log(n);
+//     }
+//     document.getElementById("resultY").innerHTML = (FibonacciValue(n - 1) + FibonacciValue(n - 2));
+
+// };
+
+// Milestone 5
+
+function fibonacciValue() {
+    const inputValue = parseInt(document.getElementById("inputX").value);
+    if (inputValue > 50) {
+        clearResult();
+        document.getElementById("myalert").style.visibility = "visible";
+        document.getElementById("inputX").style.border = "1px solid #D9534F";
+    } else {
+        clearResult();
+        loaderOn();
+        fetch("http://localhost:5050/fibonacci/" + inputValue).then(response => {
+            if (response.ok) {
+                response.json().then(data => {
+                    let answer = data.result;
+                    loaderOff();
+                    document.getElementById("resultY").innerHTML = answer;
+                    console.log(answer);
+                    console.log(data);
+                });
+            } else {
+                response.text().then(text => {
+                    console.log(text);
+                    document.getElementById("loadRing").style.visibility = "hidden";
+                    document.getElementById("resultY").innerHTML = "Server error: " + text;
+                    document.getElementById("resultY").classList.add("error-result");
+                });
+            }
+            console.log(response);
+        });
     }
-    console.log(result);
-    document.getElementById("resultY").innerHTML = result;
+}
+const button = document.getElementById("button");
+button.addEventListener("click", fibonacciValue)
+
+
+
+function loaderOn() {
+    document.getElementById("loadRing").style.visibility = "visible";
+}
+
+function loaderOff() {
+    document.getElementById("loadRing").style.visibility = "hidden";
+    document.getElementById("resultY").innerHTML = " ";
+}
+
+function clearResult() {
+    document.getElementById("resultY").innerHTML = " ";
+    document.getElementById("myalert").style.visibility = "hidden";
+    document.getElementById("inputX").style.border = "1px solid black";
+    document.getElementById("resultY").classList.remove("error-result");
 }
