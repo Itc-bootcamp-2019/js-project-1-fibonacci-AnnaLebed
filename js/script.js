b6bb7aadfc19550b7cc89f6343d90c5f78eb5d10
Anna <annettlebid@gmail.com>
2020-03-13 19:38:44 +0200

    Milestone 7

--- js/script.js
+++ js/script.js
@@ -64,17 +64,13 @@
 // };
 
 
-// ************************************************************************************
-
-// Milestone 5-6
-// 
 
 function fibonacciValue() {
+    clearResult();
     const inputValue = parseInt(document.getElementById("inputX").value);
     if (inputValue > 50) {
-        clearResult();
-        document.getElementById("myalert").style.visibility = "visible";
-        document.getElementById("inputX").style.border = "1px solid #D9534F";
+        document.getElementById("myalert").classList.add("visibility-vis");
+        document.getElementById("inputX").classList.add("border-red");
     } else {
         clearResult();
         loaderOn();
@@ -86,57 +82,103 @@ function fibonacciValue() {
                     document.getElementById("resultY").innerHTML = answer;
                     console.log(answer);
                     console.log(data);
+                    fibonacciResults();
                 });
             } else {
                 response.text().then(text => {
                     console.log(text);
-                    document.getElementById("loadRing").style.visibility = "hidden";
+                    document.getElementById("loadRing").classList.add("visibility-hid");
                     document.getElementById("resultY").innerHTML = "Server error: " + text;
                     document.getElementById("resultY").classList.add("error-result");
                 });
             }
             console.log(response);
-        });
+        })
     }
 }
 const button = document.getElementById("button");
-button.addEventListener("click", fibonacciValue);
-// button.addEventListener("click", fibonacciResults);
+button.addEventListener("click", fibonacciValue, fibonacciResults);
 
 
 
 function loaderOn() {
-    document.getElementById("loadRing").style.visibility = "visible";
+    document.getElementById("loadRing").classList.add("visibility-vis");
 }
 
 function loaderOff() {
-    document.getElementById("loadRing").style.visibility = "hidden";
+    document.getElementById("loadRing").classList.add("visibility-hid");
     document.getElementById("resultY").innerHTML = " ";
 }
 
 function clearResult() {
     document.getElementById("resultY").innerHTML = " ";
-    document.getElementById("myalert").style.visibility = "hidden";
-    document.getElementById("inputX").style.border = "1px solid black";
+    document.getElementById("inputX").classList.add("border-on");
     document.getElementById("resultY").classList.remove("error-result");
 }
 
-// *************************************************************************
 
-// Milestone 7
 
 function fibonacciResults() {
-    // loaderOn();
+    document.getElementById("loadRing").classList.add("visibility-hid");
+    lowerLoaderOn();
     fetch("http://localhost:5050/getFibonacciResults")
         .then(response => {
             return response.json();
         })
         .then(data => {
             console.log(data);
-        })
-    let getResult = JSON.stringify(data)
-    document.getElementById("results").innerHTML = getResult;
+            let response = data;
+            lowerLoaderOff();
+
+            for (let i = 0; i < response.results.length; i++) {
+                console.log(response.results[i].number);
+                console.log(response.results[i].result);
+                console.log(Date(response.results[i].createdDate));
+
+                const wrapperDiv = document.createElement('div');
+                wrapperDiv.classList.add("wrapper");
+
+                const divFib = document.createElement('div');
+                divFib.innerHTML = "The Fibonacci Of";
+
+                const divNumber = document.createElement('div');
+                divNumber.classList.add("bold");
+                divNumber.classList.add("padding");
+                divNumber.innerHTML = response.results[i].number;
+
+                const divIs = document.createElement('div');
+                divIs.innerHTML = "is";
+
+                const divResult = document.createElement('div');
+                divResult.classList.add("bold");
+                divResult.classList.add("padding-left");
+                divResult.innerHTML = response.results[i].result;
+
+                const divCalc = document.createElement('div');
+                divCalc.innerHTML = ". Calculated at: ";
+
+                const divDate = document.createElement('div');
+                divDate.classList.add("padding");
+                divDate.innerHTML = new Date(response.results[i].createdDate);
+
+                wrapperDiv.append(divFib, divNumber, divIs, divResult, divCalc, divDate);
+                const row = document.getElementById("resultsRow");
+                row.append(wrapperDiv);
+            };
+
+        });
+
 };
 
 
-button.addEventListener("click", fibonacciResults);
+function lowerLoaderOn() {
+    document.getElementById("loadRingLower").classList.add("visibility-vis");
+};
+
+function lowerLoaderOff() {
+    document.getElementById("loadRingLower").classList.add("visibility-hid");
+};
+
+
+var x = document.createElement("INPUT");
+x.setAttribute("type", "checkbox");
